ac491208ec33ce6d873ade9057f20171da99c418
Mariahl96 <mariahlewis96@gmail.com>
2026-04-10 09:23:16 -0500

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Teacher2Corp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Teacher2Corp />
+  </React.StrictMode>
+);
